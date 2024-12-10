d8638194d244746b50db78671d5d7d17e0b132c6
Trey Dedman <treydedman@gmail.com>
2024-12-10 04:43:10 +0000

    completed typescript-operators-and-expressions

---
+++ typescript-operators-and-expressions/main.js
@@ -0,0 +1,67 @@
+'use strict';
+// 1.
+// Declare two variables named width and height and assign each of them a number value
+const width = 4;
+const height = 5;
+// multiply width and height and assign the result to a new variable named area
+const area = width * height;
+// log the value of area to the console
+console.log('the value of area: ', area);
+// log the typeof area to the console
+console.log('the typeof area: ', typeof area);
+// 2.
+// Declare two variables named bill and payment and assign each of them a number value. payment should be a larger number than bill
+const bill = 20;
+const payment = 25;
+// subtract bill from payment and assign the result to a new variable named change
+const change = payment - bill;
+// log the value of change to the console
+console.log('the value of change: ', change);
+// log the typeof change to the console
+console.log('the typeof change: ', typeof change);
+// 3.
+// Declare three variables named quizzes, midterm, and final and assign each of them a number value
+const quizzes = 92;
+const midterm = 85;
+const final = 93;
+// calculate the average of quizzes, midterm, and final in one expression and assign the result to a new variable named grade
+const grade = (quizzes + midterm + final) / 3;
+// log the value of grade to the console
+console.log('the value of grade: ', grade);
+// log the typeof grade to the console
+console.log('the typeof grade: ', typeof grade);
+// Declare two variables named firstName and lastName and assign each of them to your first and last name as strings, respectively
+const firstName = 'Trey';
+const lastName = 'Dedman';
+// use string concatenation on firstName and lastName to assign the result to a new variable named fullName
+const fullName = firstName + ' ' + lastName;
+// log the value of fullName to the console
+console.log('the value of fullName: ', fullName);
+// log the typeof fullName to the console
+console.log('the typeof fullName: ', typeof fullName);
+// 5.
+// Declare a variable named pH and assign it a number value between 1 and 14 inclusive
+const pH = 8;
+// use less-than comparison to compare pH to the number 7 and assign the result to a new variable named isAcidic
+const isAcidic = pH < 8;
+// log the value of isAcidic to the console
+console.log('the value if isAcidic: ', isAcidic);
+// log the typeof isAcidic to the console
+console.log('the typeof isAcidic: ', typeof isAcidic);
+// 6.
+// Declare a variable named headCount and assign it a number value
+const headCount = 200;
+// use equality comparison to compare headCount to the number 300 and assign the result to a new variable named isSparta
+const isSparta = headCount === 300;
+// log the value of isSparta to the console
+console.log('the value of isSparta: ', isSparta);
+// log the typeof isSparta to the console
+console.log('the typeof isSparta: ', typeof isSparta);
+// 7.
+// Declare a variable named motto and assign it to the value stored in the variable fullName
+// and use addition assignment to concatenate the string ' is the GOAT' onto motto and store the result back in motto
+const motto = fullName + ' is the GOAT';
+// log the value of motto to the console
+console.log('the value of motto: ', motto);
+// log the typeof motto to the console
+console.log('the typeof motto: ', typeof motto);
---
+++ typescript-operators-and-expressions/typescript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,54 @@
+# javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What data type is returned by an arithmetic operation?
+
+numbers but the '+' is also used to concatenate strings
+
+- What is string concatenation?
+
+Joins two or more string values
+
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+
+arithmetic operations as well as concatenation
+
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+
+boolean
+
+- What does the `+=` "plus-equals" operator do?
+
+it adds the value on the right to the value of the variable on the left,
+then assigns the result back to the variable on the left
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
