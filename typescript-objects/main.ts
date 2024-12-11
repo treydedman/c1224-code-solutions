65c8278a8ab3e992332ecc385b121ac9d5bb45b1
Trey Dedman <treydedman@gmail.com>
2024-12-11 00:14:47 +0000

    completed typescript-objects

---
+++ typescript-objects/main.ts
@@ -0,0 +1,115 @@
+// student object
+// 1.
+// define an interface named StudentProps
+interface StudentProps {
+  firstName: string;
+  lastName: string;
+  age: number;
+  livesInIrvine?: boolean;
+  previousOccupation?: string;
+}
+
+// 2.
+// Using literal notation, create an object with three properties: firstName, lastName and age
+const student: StudentProps = {
+  firstName: 'Trey',
+  lastName: 'Dedman',
+  age: 21,
+};
+
+// 3.
+// Use dot notation to access firstName and lastName of student object and concatenate them into a new string assigned to a variable named fullName
+const fullName: string = `${student.firstName} ${student.lastName}`;
+
+// Log the value of fullName to the console
+console.log('the value of fullName: ', fullName);
+
+// 4.
+// Use dot notation to add a property to the student object- livesInIrvine and set to a boolean value
+student.livesInIrvine = false;
+
+// Use dot notation to add a property to the student object- previousOccupation and set to a string value
+student.previousOccupation = 'Malware Researcher';
+
+// Use dot notation to log the livesInIrvine property of the student object to the console
+console.log('the value of student.livesInIrvine: ', student.livesInIrvine);
+
+// Use dot notation to log the previousOccupation property of the student object to the console
+console.log(
+  'the value of student.previousOccupation: ',
+  student.previousOccupation
+);
+
+// Log the value of the entire student object to the console
+console.log('the value of student: ', student);
+
+// vehicle object
+// 1.
+// Define a Vehicle interface
+interface Vehicle {
+  make: string;
+  model: string;
+  year: number;
+  color?: string;
+  isConvertible?: boolean;
+}
+
+// 2.
+// Using literal notation create an object with make model and year assigned to variable name vehicle
+const vehicle: Vehicle = {
+  make: 'International',
+  model: 'Travelall',
+  year: 1962,
+};
+
+// 3.
+// Use bracket notation to add a color property to the vehicle object
+vehicle['color'] = 'Olive';
+
+// 4.
+// // Use bracket notation to add a isConvertible property to the vehicle object
+vehicle['isConvertible'] = false;
+
+// 5.
+// Use bracket notation to log the 'color' property of the vehicle object to the console
+console.log('the value of vehicle[color]: ', vehicle['color']);
+
+// 6.
+// Use bracket notation to log the 'isConvertible' property of the vehicle object to the console
+console.log('the value of vehicle[isConvertible]: ', vehicle['isConvertible']);
+
+// 7.
+// Log the value of the entire vehicle object to the console
+console.log('the value of vehicle: ', vehicle);
+
+// 8.
+// Log the typeof vehicle to the console
+console.log('the typeof vehicle: ', typeof vehicle);
+
+// pet object
+// 1.
+// Define a Pet interface
+interface Pet {
+  name?: string;
+  kind?: string;
+}
+
+// 2.
+// Using literal notation, create an object with two properties: name and kind
+// Assign the object to a new variable named pet. Annotate the object with the Pet interface
+const pet: Pet = {
+  name: 'Ruby',
+  kind: 'lab',
+};
+
+// Use the delete operator to remove the name property from the pet object
+delete pet['name'];
+
+// Use the delete operator to remove the kind property from the pet object
+delete pet.kind;
+
+// Log the value of the pet object to the console
+console.log('the value of pet: ', pet);
+
+// Log the typeof pet to the console
+console.log('the typeof pet: ', typeof pet);
---
+++ typescript-objects/typescript-objects-quiz-notes.md
@@ -0,0 +1,67 @@
+# typescript-objects-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What are objects used for?
+
+store data that is usually related
+
+- What are object properties?
+
+the association of a key or name and a value (similar to a descriptor)
+
+- Describe object literal notation.
+
+the most common and straightforward way to create an object,
+define the key and immediately assign the value of the properties in curly braces
+separated by commas
+
+- How do you remove a property from an object?
+
+the delete operator
+
+- What are the two ways to get or update the value of a property?
+
+dot notation and bracket notation
+
+- What is the purpose of interfaces in TypeScript?
+
+to annotate objects - define the shape
+
+- What are two different ways to describe the shape of an object in TypeScript?
+
+interface and type
+
+- How do optional properties in a TypeScript interface differ from mandatory properties, and why are they useful?
+
+they are optional and not required or mandatory, and allow the dev or user to enter only the required values/input and have optional values/input assigned to those that aren't required. it also allows the property to be deleted without the compiler spitting out errors.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
