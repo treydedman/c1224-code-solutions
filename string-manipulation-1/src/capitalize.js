33382bb8e8258ccda9c2a23835c010529c320cd1
Trey Dedman <treydedman@gmail.com>
2025-01-04 03:01:44 +0000

    completed string-manipulation-1

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,5 @@
+'use strict';
+/* exported capitalize */
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
---
+++ string-manipulation-1/src/reverse-word.js
@@ -0,0 +1,10 @@
+'use strict';
+/* exported reverseWord */
+function reverseWord(word) {
+  let reversed = '';
+  // had to get help with this part also - got a little confused
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word.charAt(i);
+  }
+  return reversed;
+}
